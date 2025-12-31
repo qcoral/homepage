fe25ec0d66abf03cf3069a2b55c6feec4a6406da
qcoral <41168529+qcoral@users.noreply.github.com>
2025-12-31 00:50:30 -0500

    fixed deploy dir

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ git fetch origin main
 if [ "$(git rev-parse HEAD)" != "$(git rev-parse origin/main)" ]; then
     echo "new commit detected!! pulling changes and rebuilding :3"
     git pull origin main
+    cd site
     npm run build
     echo "new site built!!1!1! lets hope prod didn't break"
 else
