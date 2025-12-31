2ffb6c400b622869e23da55de6b3921c6fd2244f
qcoral <41168529+qcoral@users.noreply.github.com>
2025-12-31 00:51:43 -0500

    fixed branches

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 # Check if new commit - if there is, pull new changes and rebuild
 
 git fetch origin main
-if [ "$(git rev-parse HEAD)" != "$(git rev-parse origin/main)" ]; then
+if [ "$(git rev-parse HEAD)" != "$(git rev-parse origin/astro-ver)" ]; then
     echo "new commit detected!! pulling changes and rebuilding :3"
-    git pull origin main
+    git pull origin astro-ver
     cd site
     npm run build
     echo "new site built!!1!1! lets hope prod didn't break"
