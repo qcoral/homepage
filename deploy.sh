f1ad54976021f6123979bd08ed54b67f0263ad3e
qcoral <41168529+qcoral@users.noreply.github.com>
2026-01-31 12:05:57 -0500

    hardcoded node binary

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ if [ "$(git rev-parse HEAD)" != "$(git rev-parse origin/astro-ver)" ]; then
     killall MainThread
     npm run build
     echo "new site built!!1!1! lets hope prod didn't break"
-    HOST=0.0.0.0 PORT=8080 node ./dist/server/entry.mjs
+    HOST=0.0.0.0 PORT=8080 /root/.nvm/versions/node/v25.2.1/bin/node ./dist/server/entry.mjs
 else
     echo "no new commits detected D:"
 fi
