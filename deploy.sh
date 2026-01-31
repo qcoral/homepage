b0ab8a7ddc673734e0a8e07db84916d05bf4360d
qcoral <41168529+qcoral@users.noreply.github.com>
2026-01-31 12:25:44 -0500

    updated script to use pnpm

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,12 @@ git fetch origin astro-ver
 if [ "$(git rev-parse HEAD)" != "$(git rev-parse origin/astro-ver)" ]; then
     echo "new commit detected!! pulling changes and rebuilding :3"
     git reset --hard origin/astro-ver
-    cd site
+    echo "$PWD"
+    cd site/
+    echo "$PWD"
     killall MainThread
-    npm run build
+    pnpm install
+    pnpm run build
     echo "new site built!!1!1! lets hope prod didn't break"
     HOST=0.0.0.0 PORT=8080 /root/.nvm/versions/node/v25.2.1/bin/node ./dist/server/entry.mjs
 else
