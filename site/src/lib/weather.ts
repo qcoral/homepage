a20792836c3ef077a7ef5fdd6a0b92bc2875743d
qcoral <41168529+qcoral@users.noreply.github.com>
2026-01-31 11:46:15 -0500

    added apparent temperature info

--- site/src/lib/weather.ts
+++ site/src/lib/weather.ts
@@ -1,11 +1,11 @@
 const WEATHER_API_ENDPOINT =
-    "https://api.open-meteo.com/v1/forecast?latitude=43.7064&longitude=-79.3986&current=is_day,temperature_2m,relative_humidity_2m,precipitation,rain,weather_code,snowfall&timezone=America%2FNew_York&forecast_days=1";
+  "https://api.open-meteo.com/v1/forecast?latitude=43.7064&longitude=-79.3986&current=is_day,temperature_2m,relative_humidity_2m,precipitation,rain,weather_code,snowfall,apparent_temperature&timezone=America%2FNew_York&forecast_days=1";
 
 export async function getWeather() {
-    console.log(`Fetching weather data`);
+  console.log(`Fetching weather data`);
 
-    const response = await fetch(`${WEATHER_API_ENDPOINT}`);
-    const data = await response.json();
-    console.log("Weather data fetched:", data);
-    return data;
+  const response = await fetch(`${WEATHER_API_ENDPOINT}`);
+  const data = await response.json();
+  console.log("Weather data fetched:", data);
+  return data;
 }
