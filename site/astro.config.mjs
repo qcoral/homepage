8b9e12ca5c71a9e48b459c3f7c37cc82066c8335
Alex Ren <41168529+qcoral@users.noreply.github.com>
2026-03-06 00:44:04 -0500

    added redirects

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -10,28 +10,32 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()],
-      server: {
-          allowedHosts: [".ngrok-free.app", ".ngrok.io"],
-      },
-  },
+    redirects: {
+        "/collectibles": "https://github.com/qcoral/collectibles",
+        "/donate": "https://hcb.hackclub.com/donations/start/alex-s-fund",
+    },
+    vite: {
+        plugins: [tailwindcss()],
+        server: {
+            allowedHosts: [".ngrok-free.app", ".ngrok.io"],
+        },
+    },
 
-  markdown: {
-      rehypePlugins: [
-          [
-              rehypeExternalLinks,
-              {
-                  target: "_blank",
-                  rel: ["noopener", "noreferrer"],
-              },
-          ],
-      ],
-  },
+    markdown: {
+        rehypePlugins: [
+            [
+                rehypeExternalLinks,
+                {
+                    target: "_blank",
+                    rel: ["noopener", "noreferrer"],
+                },
+            ],
+        ],
+    },
 
-  adapter: node({
-      mode: "standalone",
-  }),
+    adapter: node({
+        mode: "standalone",
+    }),
 
-  integrations: [mdx()],
+    integrations: [mdx()],
 });
